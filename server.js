6690dc126f89b6089b2449c175dd53e1a50405e9
Adam Jones <machieajones@icloud.com>
2018-09-26 15:35:11 +0100

    Episode 7

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const app = express();
 const PORT = 4444;
 
@@ -6,19 +7,69 @@ const PORT = 4444;
 const Blog = require('./models/Blog');
 const User = require('./models/User');
 
+const typeDefs = gql`
+	
+	type Blog {
+		title: String!
+		author: String
+		body: String
+		date: String
+	}
+
+	type User {
+		username: String!
+		password: String!
+		email: String!
+		date: String
+	}
+
+	type Query {
+		showAllBlogs: [Blog]
+	}
+`;
+
+const resolvers = {
+	Query: () => {}
+}
+
 // mongoose 
 const mongoose = require('mongoose');
 
 require('dotenv').config({ path: 'variables.env'});
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI, { 
+			useNewUrlParser: true,
+			useCreateIndex: true
+		})
 		.then(() => console.log('db connected'))
 		.catch(err => console.error(err));
 
-app.listen(PORT, () => {
-	console.log('I am listening');
+const server = new ApolloServer({ 
+	typeDefs, 
+	resolvers,
+	context: {
+		Blog,
+		User
+	}
 });
+
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () =>
+  console.log(`🚀 Server ready at http://localhost:4444${server.graphqlPath}`)
+)
 
 app.get('/', function(req, res) {
 	res.send('Hello World');
 });
+
+
+
+
+
+
+
+
+
+
+
